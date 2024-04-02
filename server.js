f27b6c869f002dc012017af66bb515d8cf3acd63
anyols <anyols.arnau@gmail.com>
2024-04-03 00:24:05 +0200

    port

--- server.js
+++ server.js
@@ -45,4 +45,7 @@ app.post("/create-checkout-session", async (req, res) => {
   }
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || "3000";
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
